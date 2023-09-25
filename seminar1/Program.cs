ff3145632559ee1766aec09bce87249ad80be3f1
Kate Zubareva <katerina.vinograd@gmail.com>
2023-09-26 00:27:33 +0300

    task 2

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -3,21 +3,49 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a < b)
+// {
+//     Console.Write("меньшее: ");
+//     Console.WriteLine(a);
+//     Console.Write("большее: ");
+//     Console.WriteLine(b);
+// }
+// else
+// {
+//     Console.Write("меньшее: ");
+//     Console.WriteLine(b);
+//     Console.Write("большее: ");
+//     Console.WriteLine(a);
+// }
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
 Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a < b)
+Console.Write("Введите число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
 {
-    Console.Write("меньшее: ");
-    Console.WriteLine(a);
-    Console.Write("большее: ");
-    Console.WriteLine(b);
+    max = b;
+}
+else if (c > max)
+{
+    max = c;
 }
 else
 {
-    Console.Write("меньшее: ");
-    Console.WriteLine(b);
-    Console.Write("большее: ");
-    Console.WriteLine(a);
+    max = a;
 }
+Console.Write("max: ");
+Console.WriteLine(max);
