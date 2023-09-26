6ec4068ee154f858d25c23dad029ff8e8eddabe4
Kate Zubareva <katerina.vinograd@gmail.com>
2023-09-26 23:24:42 +0300

    task 2 correct

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -28,24 +28,15 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число: ");
-int c = Convert.ToInt32(Console.ReadLine());
-int max = a;
-if (b > max)
-{
-    max = b;
-}
-else if (c > max)
-{
-    max = c;
-}
-else
-{
-    max = a;
-}
-Console.Write("max: ");
-Console.WriteLine(max);
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > max) max=a;
+// if (b > max) max=b;
+// if (c > max) max=c;
+// Console.Write("max: ");
+// Console.WriteLine(max);
