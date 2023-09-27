1064440ecaaef160f921015cf9da9c48de745428
Kate Zubareva <katerina.vinograd@gmail.com>
2023-09-27 22:22:54 +0300

    task 4

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -9,17 +9,13 @@
 // int b = Convert.ToInt32(Console.ReadLine());
 // if (a < b)
 // {
-//     Console.Write("меньшее: ");
-//     Console.WriteLine(a);
-//     Console.Write("большее: ");
-//     Console.WriteLine(b);
+//     Console.WriteLine("меньшее: " + a);
+//     Console.WriteLine("большее: " + b);
 // }
 // else
 // {
-//     Console.Write("меньшее: ");
-//     Console.WriteLine(b);
-//     Console.Write("большее: ");
-//     Console.WriteLine(a);
+//     Console.WriteLine("меньшее: " + b);
+//     Console.WriteLine("большее: " + a);
 // }
 
 
@@ -38,21 +34,30 @@
 // if (a > max) max=a;
 // if (b > max) max=b;
 // if (c > max) max=c;
-// Console.Write("max: ");
-// Console.WriteLine(max);
+// Console.WriteLine("max: " + max);
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a % 2 == 0)
-{
-    Console.Write("число является четным");
-}
-else
-{
-    Console.Write("число является нечетным");
-}
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+// {
+//     Console.Write("число является четным");
+// }
+// else
+// {
+//     Console.Write("число является нечетным");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.Write("Введите число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= N; i++)
+//     if (i % 2 == 0)    
+// Console.Write(i + " ");
